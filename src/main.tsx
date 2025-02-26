0c38693d5191b2bcea60af4aa169b735d83aaf6d
ricoerian <richoeriansyahm@gmail.com>
2025-02-27 02:19:11 +0700

    push

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'antd/dist/reset.css'
-import 'antd/dist/antd.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
